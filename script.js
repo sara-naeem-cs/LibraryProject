750b96083ed75c5e5bd0f01d2c81f75727fa2620
Sara Naeem <63316988+sara-naeem-cs@users.noreply.github.com>
2023-08-18 23:56:15 -0400

    completed everything except read checkbox funcionality

--- script.js
+++ script.js
@@ -1,102 +1,138 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //Getting information to add to the library 
-    const newName = document.getElementById("book-title")
-    const newAuthor = document.getElementById("book-author")
-    const newPages = document.getElementById("pages")
-    const submitButton = document.getElementById("submit")
-    const bookContainter = document.getElementById("book-holder");
-    const deleteButtons = document.querySelectorAll(".delete"); 
-    const openFormBtn = document.getElementById('addBookBtn');
-    const closeFormBtn = document.getElementById("close-btn");
+  //Getting information to add to the library 
+  const newName = document.getElementById("book-title")
+  const newAuthor = document.getElementById("book-author")
+  const newPages = document.getElementById("pages")
+  const submitButton = document.getElementById("submit")
+  const bookContainter = document.getElementById("book-holder");
+  let deleteButtons = document.querySelectorAll(".delete");
+  const openFormBtn = document.getElementById('addBookBtn');
+  const closeFormBtn = document.getElementById("close-btn");
 
 
 
 
-    let myLibrary = [];
-    const book1 = new Book("title1", "author1", 4, true)
-    const book2 = new Book("title2", "author1", 8, true)
-    const book3 = new Book("title3", "author1", 12, true)
+  let myLibrary = [];
+  const book1 = new Book("Percy Jackson", "Rick Riordan", 350, true)
+  const book2 = new Book("Harry Potter", "J. K. Rowling", 500, true)
+  const book3 = new Book("Hunger Games", "Suzanne Collins", 300, false)
 
-    addBookToLibrary(book1);
-    addBookToLibrary(book2);
-    addBookToLibrary(book3);
+  addBookToLibrary(book1);
+  addBookToLibrary(book2);
+  addBookToLibrary(book3);
 
-    //Constructor
-    function Book(title, author, numPages, read) {
-        this.title = title
-        this.author = author
-        this.numPages = numPages
-        this.read = read
+  //Constructor
+  function Book(title, author, numPages, read) {
+    this.title = title
+    this.author = author
+    this.numPages = numPages
+    this.read = read
+  }
+
+  function addBookToLibrary(book) {
+    myLibrary.push(book);
+  }
+
+  function printBooks() {
+    for (var i = 0; i < myLibrary.length; i++) {
+      console.log(myLibrary[i]);
     }
+  }
 
-    function addBookToLibrary(book) {
-        myLibrary.push(book);
-    }
+  function removeBooks() {
+    document.querySelectorAll(".book").forEach(book => book.remove());
+  }
 
-    function printBooks(){
-        for (var i = 0; i < myLibrary.length; i++) {
-            console.log(myLibrary[i]);
-          }
-    }
+  function showAllBooks() {
+    removeBooks();
+    let i = 0;
+    myLibrary.forEach((book, i) => {
+      let status = "unchecked";
+      if (book.read) {
+        status = "checked";
+      }
 
-    function showBooks(){
-        let newContent = `<div class="book" data-book="${myLibrary.length}">
+
+      let newContent = `<div class="book" data-book="${i}">
                         <button class="delete"><span class="text">&times;</span></button>
-                        <h4>${newName.value}</h4>
-                        <p>${newAuthor.value}</p>
-                        <p>${newPages.value}</p>
-                        <button class="read"><span class="text">Read</span></button>
+                        <h4>${book.title}</h4>
+                        <p>${book.author}</p>
+                        <p>${book.numPages}</p>
+                       
+												<label for="existing-book-status">Read</label>
+                        <input type="checkbox" id="existing-book-status" name="read" value="read" ${status}>    
                         </div>`;
-        bookContainter.insertAdjacentHTML("beforeend", newContent);
+ 
+      bookContainter.insertAdjacentHTML("beforeend", newContent);
+            
+      
+      
+      
+    });
+    
+    deleteButtons = document.querySelectorAll(".delete");
+    deleteButtons.forEach((deleteButton) => {
+       deleteButton.addEventListener("click", handleClick);
+    });  
+
+  }
+
+
+  submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(newName.value);
+    console.log(newAuthor.value);
+    console.log(newPages.value);
+    let book_status = true;
+    if (document.getElementById("book-status").checked) {
+      book_status = true;
+    } else {
+      book_status = false;
     }
 
-    submitButton.addEventListener('click', (event) => {
-        event.preventDefault();
-        console.log(newName.value);
-        console.log(newAuthor.value);
-        console.log(newPages.value);
-        const book = new Book(newName.value, newAuthor.value, newPages.value, true);
-        showBooks();
-        addBookToLibrary(book)
-    });
-  
-  deleteButtons.forEach(book=> {
-    book.addEventListener("click", function(e) {
-            const clickedBook = e.target.parentElement.parentElement;
-            var index = clickedBook.getAttribute('data-type');
-              
-            clickedBook.remove();
-            //Need to remove the specific book from the array:
-            console.log(myLibrary);
-            myLibrary.splice(parseInt(index), 1);
-            console.log(myLibrary);
-      });
-  }); 
+    const book = new Book(newName.value, newAuthor.value, newPages.value, book_status);
+    addBookToLibrary(book)
+    showAllBooks();
+  });
 
-  
-openFormBtn.addEventListener('click', () => { 
-  const form = document.getElementById('myForm');
-  if (form.style.display === 'none') {
-    form.style.display = 'block';
-    openFormBtn.style.display = 'none';
-  } else {
-    form.style.display = 'none';
-    openFormBtn.style.display = 'block';
-  } 
-});
 
-  closeFormBtn.addEventListener('click', () =>{
-     const form = document.getElementById('myForm');
+
+  openFormBtn.addEventListener('click', () => {
+    const form = document.getElementById('myForm');
+    if (form.style.display === 'none') {
+      form.style.display = 'block';
+      openFormBtn.style.display = 'none';
+    } else {
+      form.style.display = 'none';
+      openFormBtn.style.display = 'block';
+    }
+  });
+
+  closeFormBtn.addEventListener('click', () => {
+    const form = document.getElementById('myForm');
     form.style.display = 'none';
     openFormBtn.style.display = 'block';
   })
   
-    //************************************************* */
+  
+
+    
+  const handleClick = (event) => {
+    
+    //console.log("helo");
+    let index = event.target.parentNode.parentNode.getAttribute("data-book");
+    //We delete this from the library and then re-render everything 
+    console.log("hello",index);
+    myLibrary.splice(parseInt(index), 1);
+    showAllBooks();
+  }
 
 
 
-    function main(){
+  Window.onload = showAllBooks();
 
-    }
-    main();   
+  function main() {
+
+  }
+  main();
 })
